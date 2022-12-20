7321def0cb132ce936153d8e911ad779c9460112
kk2211 <kohlikabir101@gmail.com>
2022-12-21 00:33:51 +0530

    responding to multiple pings

--- app/main.js
+++ app/main.js
@@ -5,9 +5,11 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  connection.on("data",()=>{
-    connection.write("+PONG\r\n");
-  });
+    //Respond to multiple PINGs: connection.on can automatically respond to multiple pings
+    // No need to create a loop
+    connection.on("data", () => {
+        connection.write("+PONG\r\n");
+    });
 });
 
 server.listen(6379, "127.0.0.1");
